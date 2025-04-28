8e8dd430e563f7ee6f81f2c18a76f484a1eb086d
maksymkulbaka <mrskylineer@gmail.com>
2025-04-29 01:35:25 +0300

    Add changes to "vector2d_example.py" file

--- vector2d_example.py
+++ vector2d_example.py
@@ -1,20 +1,19 @@
 from vector2d import Vector2D
 
-if __name__ == "__main__":
-    help(Vector2D)
+help(Vector2D) # Displays documentation about the module
 
-    vect1 = Vector2D(2,3)
-    vect2 = Vector2D(3,5)
+vect1 = Vector2D(2,3)
+vect2 = Vector2D(3,5)
 
-    print(f"Vector vect1: {vect1}")
-    print(f"Vector vect2: {vect2}")
+print(f"Vector vect1: {vect1}") # Output: {'x': 2.0, 'y': 3.0}
+print(f"Vector vect2: {vect2}") # Output: {'x': 3.0, 'y': 5.0}
 
-    print(f"vect1 + vect2: {vect1 + vect2}")
-    print(f"vect1 - vect2: {vect1 - vect2}")
-    print(f"vect1 * 3: {vect1 * 3}")
-    print(f"vect1 == vect2: {vect1 == vect2}")
+print(f"vect1 + vect2: {vect1 + vect2}") # Output: {'x': 5.0, 'y': 8.0}
+print(f"vect1 - vect2: {vect1 - vect2}") # Output: {'x': -1.0, 'y': -2.0}
+print(f"vect1 * 3: {vect1 * 3}") # Output: {'x': 6.0, 'y': 9.0}
+print(f"vect1 == vect2: {vect1 == vect2}") # Output: False
 
-    print(f"Magnitude of vect1: {vect1.mag()}")
-    print(f"Unit vector of vect1: {vect1.unit()}")
-    print(f"Dot product of vect1 and vect2: {vect1.dot(vect2)}")
-    print(f"Angle between vect1 and vect2: {vect1.angle(vect2)}")
+print(f"Magnitude of vect1: {vect1.mag()}") # Output: 3.605551275463989
+print(f"Unit vector of vect1: {vect1.unit()}") # Output: {'x': 0.5547001962252291, 'y': 0.8320502943378437}
+print(f"Dot product of vect1 and vect2: {vect1.dot(vect2)}") # Output: 21.0
+print(f"Angle between vect1 and vect2: {vect1.angle(vect2)}") # Output: 0.04758310327698479
